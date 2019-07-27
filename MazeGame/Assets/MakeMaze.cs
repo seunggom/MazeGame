c75e893449a7bbd74cd119e82699e3e0ec3ad35b
seunggom <47077179+seunggom@users.noreply.github.com>
2019-07-27 17:04:54 +0900

    미로 생성 코딩
    워 코딩 하기 싫어유

---
+++ MazeGame/Assets/MakeMaze.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Room
+{
+    public RoomInfo room_info;
+}
+
+public enum RoomInfo
+{
+    road = 0,
+    enterable = 1,
+    wall = 2,
+};
+
+public class MakeMaze : MonoBehaviour
+{
+    public int X_mazeSize = 10;
+    public int Y_mazeSize = 8;
+
+    public Room[ , ] maze;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DesignMaze();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DesignMaze()
+    {
+        this.maze = new Room[X_mazeSize, Y_mazeSize];
+    }
+}
