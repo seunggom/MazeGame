36c327ec5270254665c2c0f21a2338327391f385
seunggom <47077179+seunggom@users.noreply.github.com>
2019-07-31 18:49:06 +0900

    미로 생성 코딩 5

--- MazeGame/Assets/MakeMaze.cs
+++ MazeGame/Assets/MakeMaze.cs
@@ -45,22 +45,108 @@ public class MakeMaze : MonoBehaviour
          When: 2019/07/29
          Description: 랜덤으로 미로를 생성하는 함수. 미로 구현은 recursive backtracking 알고리즘을 사용.
          */
-        this.maze = new Room[X_mazeSize, Y_mazeSize];
-        for (int i = 0; i < X_mazeSize; i++)
+        this.maze = new Room[X_mazeSize + 2, Y_mazeSize + 2];
+        for (int i = 0; i < X_mazeSize+2; i++)
         {
-            for (int  j= 0;j < Y_mazeSize; j++)
+            for (int  j= 0;j < Y_mazeSize+2; j++)
             {
-                maze[i, j].room_info = RoomInfo.pure; // 초기 모든 방을 wall로 설정
+                if (i == 0 || i == X_mazeSize + 1 || j == 0 || j == Y_mazeSize + 1)
+                {
+                    maze[i, j].room_info = RoomInfo.end;
+                }
+                else
+                {
+                    maze[i, j].room_info = RoomInfo.pure; // 초기 모든 방을 pure로 설정
+                }
                 maze[i, j].wall = new bool[4] { true, true, true, true }; // 값이 true이면 벽이 있다는 의미. 순서대로 상하좌우임.
             }
         }
-        int x = 0;
-        int y = 0;
+        
+        int x = 1;
+        int y = 1;
         maze[x, y].room_info = RoomInfo.potential; // (0,0)은 미로 생성의 첫 시작점
         roomStack[0].Push(x);
         roomStack[1].Push(y);
 
-        int a, b, dir;
+       
+        int a, b, dir, final_dir;
+        
+       
+        while (roomStack[0].Count > 0)
+        {
+            int opportunity = 1;
+
+            a = x;
+            b = y;
+            dir = Random.Range(0, 4);
+            
+            while (opportunity <= 4)
+            {
+                switch (dir)
+                {
+                    case 0:
+                        // 상
+                        a--; break;
+                    case 1:
+                        // 하
+                        a++; break;
+                    case 2:
+                        // 좌
+                        b--; break;
+                    case 3:
+                        // 우
+                        b++; break;
+                }
+
+                if (maze[a, b].room_info == RoomInfo.pure)
+                {
+                    final_dir = dir;
+                    switch (final_dir)
+                    {
+                        case 0:
+                            // 상
+                            maze[x, y].wall[0] = false;
+                            maze[a, b].wall[1] = false;
+                            break;
+                        case 1:
+                            // 하
+                            maze[x, y].wall[1] = false;
+                            maze[a, b].wall[0] = false;
+                            break;
+                        case 2:
+                            // 좌
+                            maze[x, y].wall[2] = false;
+                            maze[a, b].wall[3] = false;
+                            break;
+                        case 3:
+                            // 우
+                            maze[x, y].wall[3] = false;
+                            maze[a, b].wall[2] = false;
+                            break;
+                    }
+
+                    roomStack[0].Push(a);
+                    roomStack[1].Push(b);
+                    x = a;
+                    y = b;
+                    break;
+
+                }
+
+                opportunity++;
+                dir++;
+                if (dir == 4) dir = 0;
+
+                if (opportunity == 5)
+                {
+                    roomStack[0].Pop();
+                    roomStack[1].Pop();
+                }
+            }
+            
+        } // 스택 비었음
+
+        /*
         int opportunity = 1;
         //bool canGo = false; // 갈 수 있는 방향이 있는지 알려주는 변수
         while (roomStack[0].Count > 0)
@@ -68,6 +154,8 @@ public class MakeMaze : MonoBehaviour
             a = x;
             b = y;
             dir = Random.Range(0, 4);
+
+
             while (opportunity <= 4)
             {
                 switch (dir)
@@ -147,6 +235,7 @@ public class MakeMaze : MonoBehaviour
 
 
         } // 미로 구현 끝
+        */
     }
 
 
@@ -157,10 +246,10 @@ public class MakeMaze : MonoBehaviour
          When: 2019/07/29
          Description: 랜덤으로 생성한 미로가 잘 구현되었는지 확인하기 위해 미로의 정보를 출력하는 함수.
          */
-        for (int i = 0; i < X_mazeSize; i++)
+        for (int i = 0; i < X_mazeSize+2; i++)
         {
             string s = "";
-            for (int j = 0; j < Y_mazeSize; j++)
+            for (int j = 0; j < Y_mazeSize+2; j++)
             {
                 s = s + maze[i, j].room_info + "(";
                 for (int k = 0; k < 4; k++) if (maze[i, j].wall[k]) s = s + k;
