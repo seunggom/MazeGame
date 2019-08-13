023e841ac41c8e8df3a9b86b799831f10034750b
minho9710 <minho9710@naver.com>
2019-08-13 15:57:00 +0900

    씬넘어가는거
    초기화면씬 만들고 씬넘어가는거 (별거음슴) 만들고 사이즈받는거 만드려다가 나온것

---
+++ MazeGame/Assets/GetMazeSize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetMazeSize : MonoBehaviour
+{
+    public InputField InputSize;
+
+    public void Input()
+    {
+        PlayerPrefs.SetInt("Size", int.Parse(InputSize.text));
+    }
+    
+} 
