7b665ea162476ffd7e057d0324785c4f819738d6
minho9710 <minho9710@naver.com>
2019-08-31 15:39:46 +0900

    카메라위치수정 마우스따라움직이는거추가
    ㅇㅇ

---
+++ MazeGame/Assets/Scenes/fpscontrol.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpscontrol : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float rotSpeed = 3.0f;
+
+    public Camera fpsCam;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        MoveCtrl();
+        RotCtrl();
+    }
+
+    void MoveCtrl()
+    { //키보드 W,S,A,D Player 이동 함수
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+    }
+
+    void RotCtrl()
+    { //마우스 회전 시점이동 함수
+        float rotX = Input.GetAxis("Mouse Y") * rotSpeed;
+        float rotY = Input.GetAxis("Mouse X") * rotSpeed;
+
+        this.transform.localRotation *= Quaternion.Euler(0, rotY, 0);
+        fpsCam.transform.localRotation *= Quaternion.Euler(-rotX, 0, 0);
+    }
+}
