422d7bd0faf249289bba28d48125443aa499b99b
seunggom <47077179+seunggom@users.noreply.github.com>
2019-09-09 17:11:47 +0900

    힌트캡슐2

--- MazeGame/Assets/Scenes/MakeMaze.cs
+++ MazeGame/Assets/Scenes/MakeMaze.cs
@@ -23,8 +23,6 @@ public class MakeMaze : MonoBehaviour
     public static int X_mazeSize = GetMazeSize.X_Size;
     public static int Y_mazeSize = GetMazeSize.Y_Size;
 
-    private short[] password = new short[4]; // 탈출을 위한 비밀 번호 4자리
-
     public Room[ , ] maze;
 
     Stack<int> roomStack0 = new Stack<int>();
@@ -36,11 +34,7 @@ public class MakeMaze : MonoBehaviour
         DesignMaze();
         PrintMaze();
         space();
-        password = MakePassword();
-        print(password[0]);
-        print(password[1]);
-        print(password[2]);
-        print(password[3]);
+        
     }
 
     // Update is called once per frame
@@ -223,28 +217,7 @@ public class MakeMaze : MonoBehaviour
         }
     }
 
-    short[] MakePassword()
-    {
-        /*
-         * Author : 김승연
-         * Description: 탈출 비밀번호를 만들기 위한 함수. 각 네자리는 중복될 수 없음.
-         * */
-        short[] pw = new short[4] { -1, -1, -1, -1 };
-
-        for(int i=0; i<4; i++)
-        {
-            pw[i] = (short)Random.Range(0, 10);
-            for (int j=0; j<i; j++)
-            {
-                while (pw[j] == pw[i])
-                {
-                    pw[i] = (short)Random.Range(0, 10);
-                }
-            }
-        }
-
-        return pw;
-    }
+   
 
 }
 
