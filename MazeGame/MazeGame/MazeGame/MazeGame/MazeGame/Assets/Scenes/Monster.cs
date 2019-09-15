a2bc3403638eb8743f3e4e1b038f33158798aca1
KDG3536 <53370827+KDG3536@users.noreply.github.com>
2019-09-15 21:48:34 +0900

    몬스터
    랜덤몬스터 생성
    >일정거리 근접하면 몬스터가 플레이어에게 3초동안 접근
    >3초 지나면 몬스터 사라지고 새로운지역에 몬스터 생김
    >몬스터에게 닿으면 게임오버(아직 미구현)

---
+++ MazeGame/MazeGame/MazeGame/MazeGame/MazeGame/Assets/Scenes/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public static int X_mazeSize = GetMazeSize.X_Size;
+    public static int Y_mazeSize = GetMazeSize.Y_Size;
+    private Rigidbody mobRigid;
+    public GameObject mob;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mob();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Mob()
+    {
+        
+            Instantiate(mob, new Vector3(0.5f + Random.Range(X_mazeSize / 2, X_mazeSize), 0.5f, 0.5f + Random.Range(Y_mazeSize / 2, Y_mazeSize)), Quaternion.identity);
+        
+    }
+}
---
+++ MazeGame/MazeGame/MazeGame/MazeGame/MazeGame/Assets/Scenes/TimePassed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimePassed : MonoBehaviour
+{
+    public float TimePass;
+    public Text Text_Timer;
+
+    void Update()
+    {
+        TimePass += Time.deltaTime;
+        Text_Timer.text = "Time : " + Mathf.Floor(TimePass*100)/100;
+    }
+}
