84cdab33a8cb67a3c3a486035d6b5c35c8304a35
seunggom <47077179+seunggom@users.noreply.github.com>
2019-11-10 17:39:50 +0900

    Update ActivateCheck.cs

--- MazeGame/Assets/Scenes/Maze_script/ActivateCheck.cs
+++ MazeGame/Assets/Scenes/Maze_script/ActivateCheck.cs
@@ -2,43 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ActivateCheck : MonoBehaviour
 {
     public GameObject CheckUI;
+    public GameObject Player;
+    public Vector3 pos;
+    public Vector3 start;
+    public float temp;
     bool IsChecking;
     // Start is called before the first frame update
     void Start()
     {
         CheckUI.SetActive(false);
         IsChecking = false;
+        Vector3 start = new Vector3(1.5f, 0.5f, 1.5f);
+        Player = GameObject.Find("Player(Clone)");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F1))
+        pos = this.Player.transform.position;
+        Debug.Log("위치 " + temp);
+        temp = Vector3.Distance(pos, start);
+        if (temp < 2.5)
         {
-            if (IsChecking == false && Pause.IsPause==false)
+            if (Input.GetKeyDown(KeyCode.F1))
             {
-                IsChecking = true;
-                CheckUI.SetActive(true);
-                return;
+                if (IsChecking == false && Pause.IsPause == false)
+                {
+                    IsChecking = true;
+                    CheckUI.SetActive(true);
+                    return;
+                }
+                if (IsChecking == true)
+                {
+                    IsChecking = false;
+                    CheckUI.SetActive(false);
+                    return;
+                }
             }
-            if (IsChecking == true)
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                IsChecking = false;
-                CheckUI.SetActive(false);
-                return;
-              }
+                if (IsChecking == true)
+                {
+                    CheckUI.SetActive(false);
+                    IsChecking = false;
+                    return;
+                }
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
+        else
         {
-            if (IsChecking == true)
-            { 
-               CheckUI.SetActive(false);
-               IsChecking = false;
-               return;
-            }
+            IsChecking = false;
+            CheckUI.SetActive(false);
+            return;
         }
     }
 }
